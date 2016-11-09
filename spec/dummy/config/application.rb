fbe07f9758238709a209bdc7abf766c19c443106
Moritz E. Beber <midnighter@posteo.net>
2016-11-09 19:34:21 +0100

    require full rails dummy application for now

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,14 +1,15 @@
 require_relative 'boot'
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
-require "active_job/railtie"
-require "action_cable/engine"
-# require "rails/test_unit/railtie"
-require "sprockets/railtie"
+require 'rails/all'
+## Pick the frameworks you want:
+#require "active_record/railtie"
+#require "action_controller/railtie"
+#require "action_view/railtie"
+#require "action_mailer/railtie"
+#require "active_job/railtie"
+#require "action_cable/engine"
+## require "rails/test_unit/railtie"
+#require "sprockets/railtie"
 
 Bundler.require(*Rails.groups)
 require "geo"
